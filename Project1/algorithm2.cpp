485e76de3d102766163f330a58ae2751d4b55b80
Bak Jisoo <48276691+jisoo449@users.noreply.github.com>
2020-06-09 15:42:23 +0900

    알골2

---
+++ Project1/algorithm2.cpp
@@ -0,0 +1,9 @@
+#include<string>
+#include<fstream>
+#include"algorithm2.h"
+
+/*al2::al2(string ref,string sh) : ref(ref), sh(sh) {
+	shortread.open(sh);
+}*/
+void algorithm2::readfile() {
+}
--- Project1/algorithm2.h
+++ Project1/algorithm2.h
@@ -1,1 +1,19 @@
 #pragma once
+#ifndef _ALGORITHM2_H
+#define _ALGORITHM2_H
+using namespace std;
+class algorithm2 {
+	string ref, sh;
+	istream fin1, fin2;//fin1: shortread, fin2: reference
+	ofstream fout;//restructed
+public:
+	algorithm2() {
+		
+	}
+	algorithm2(string ref, string sh) : ref(ref), sh(sh) {
+		fin1.open(sh);
+		fin2.open(ref);
+	}
+	void readfile();
+};
+#endif

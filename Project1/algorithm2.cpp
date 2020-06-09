7ed13e912a6c89955b894f9b3fb0dbc2240141b4
Bak Jisoo <48276691+jisoo449@users.noreply.github.com>
2020-06-09 17:40:57 +0900

    sp테이블 구현, kmp로 비교

--- Project1/algorithm2.cpp
+++ Project1/algorithm2.cpp
@@ -1,9 +1,90 @@
 #include<string>
 #include<fstream>
+#include<vector>
 #include"algorithm2.h"
 
-/*al2::al2(string ref,string sh) : ref(ref), sh(sh) {
-	shortread.open(sh);
-}*/
-void algorithm2::readfile() {
+
+algorithm2::algorithm2(int k, int n, string reference, string shortread, string mydna) :reference(reference), shortread(shortread), mydna(mydna) {
+	this->n = n;
+	this->k = k;
+
+	idx = new int[n];
+	loc = new char*[n];
+	for (int i = 0; i < n; i++)loc[i] = new char[k + 1];
+}
+
+void algorithm2::computeSP(char* str, int* sp, int size) {
+	int K = -1;
+	sp[0] = -1;
+	for (int i = 0; i < size; i++) {
+		while (K >= 0 && str[K + 1] != str[i])K = sp[K];
+		if (str[K + 1] == str[i])K++;
+		sp[i] = K;
+	}
+}
+
+void algorithm2::reconstruct() {
+
+	fout.open(mydna);
+	fin1.open(shortread);
+	fin2.open(reference);
+
+	char * str1 = new char[k], *str2 = new char[k];//str1:shortread를 저장, str2:reference를 저장
+	int tablesize = k / 3;
+	int *sp = new int[tablesize];
+
+	while (fin1.getline(str1, k)) {
+		int j = -1;
+		int t = 0;
+		int mismatch = 0;//다른 문자의 개수
+
+		computeSP(str1, sp, tablesize);//sp 테이블 생성
+
+		while (fin2.get(str2, k)) {
+			for (int i = 0; i < k - 1; i++) {
+				//kmp알고리즘 이용하여 특정 부분 중 mismatch이하로 일치하는지 찾아보기
+				while (j >= 0 && str1[j + 1] != str2[i]) j = sp[j];
+				if (str1[j + 1] != str2[i])mismatch++;
+				else if (str1[j + 1] == str2[i] && mismatch <= d) {
+					j++;
+				}
+				else if (mismatch > d)break;
+
+				//일치한다면 제대로 비교
+				if (j == tablesize - 1) {
+					//전체 문자열 가져와서 비교하기(인덱스j부터)
+					//
+				}
+			}
+		}
+
+
+		while (fin2.getline(str2, k)) {
+
+			for (int j = 0; j < k; j++) {
+				if (str1[j] != str2[j]) n++;
+				if (n > 2) break;
+			}
+			if (n == 2 || n == 0) {
+				idx[t] = i;
+				strcpy(loc[t++], str1);
+			}
+			fin2.seekg(++i, ios::beg);
+		}
+
+		memset(sp, NULL, k);
+	}
+
+	for (int i = 0; i < n; i++) {
+
+	}
+
+	fout.close();
+
+}
+
+void algorithm2::compare() {
+
+
+
 }
--- Project1/algorithm2.h
+++ Project1/algorithm2.h
@@ -2,18 +2,21 @@
 #ifndef _ALGORITHM2_H
 #define _ALGORITHM2_H
 using namespace std;
+
 class algorithm2 {
-	string ref, sh;
-	istream fin1, fin2;//fin1: shortread, fin2: reference
-	ofstream fout;//restructed
+	ofstream fout;//fout:mydna
+	ifstream fin1, fin2;//fin1:shortread, fin2: reference 
+	int n, k, d; //n:shortread개수, k:shortread길이, d:허용하는 mismatch 개수
+	string shortread, mydna, reference;
+	int x = 2;
+	int *idx;
+	char **loc;
+
 public:
-	algorithm2() {
-		
-	}
-	algorithm2(string ref, string sh) : ref(ref), sh(sh) {
-		fin1.open(sh);
-		fin2.open(ref);
-	}
-	void readfile();
+	algorithm2(int k, int n, string reference = "reference.txt", string shortread = "shortread.txt", string mydna = "mydna.txt");
+	void reconstruct();
+	void compare();
+	void computeSP(char* str, int* sp, int size);
+	void KMP();
 };
 #endif

5fcdb41866b8641e366a75d4e4886606695d4942
Bak Jisoo <48276691+jisoo449@users.noreply.github.com>
2020-06-16 14:49:29 +0900

    필요없는 주석 삭제

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -11,57 +11,6 @@
 #include"shortread.h"
 #include"reference.h"
 using namespace std;
-/*typedef long clock_t;
-#define CLOCK_PER_SEC ((clock_t)1000)
-vector<int> getPi(string p){ 
-	int m = (int)p.size(), j=0;
-	vector<int> pi(m, 0);
-	for(int i = 1; i< m ; i++){
-		while(j > 0 && p[i] != p[j]) j = pi[j-1]; 
-		if(p[i] == p[j]) pi[i] = ++j; 
-	} return pi;
-}
-
-vector<int> kmp(string s, string p){
-	vector<int> ans; 
-	auto pi = getPi(p); 
-	int n = (int)s.size(), m = (int)p.size(), j =0;
-	for(int i = 0 ; i < n ; i++){ 
-		while(j>0 && s[i] != p[j]) j = pi[j-1]; 
-		if(s[i] == p[j]){
-			if(j==m-1){
-				ans.push_back(i-m+1); 
-				j = pi[j]; 
-			}
-			else{
-				j++; 
-			}
-		}
-	}
-	return ans; 
-} 
-
-int main(){	string s, p;
-	clock_t start, end;
-	getline(cin, s); 
-	getline(cin, p); 
-
-	start = clock();
-	cout << s.find(p) << endl;
-	end = clock();
-	cout << end - start << endl;
-
-	start = clock();
-	auto matched = kmp(s,p);
-	printf("%d\n", (int)matched.size());
-	end = clock();
-
-	for(auto i : matched) printf("%d ", i+1);
-	cout <<endl<< end - start << endl << endl;
-
-<<<<<<< HEAD
-}
-*/
 
 int main(){ 
 

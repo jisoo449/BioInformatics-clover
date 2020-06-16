311e9d506ad2591a27ce59069f9a08616273fce5
Moonsu9158 <62553959+Moonsu9158@users.noreply.github.com>
2020-06-16 17:15:48 +0900

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -23,23 +23,28 @@ int main() {
 	cout << "shortread 개수 ?";
 	int shortnum;
 	cin >> shortnum;
+
 	string *arr = new string[shortnum];
 	for (int i = 0; i < shortnum; i++) { //쇼트리드를 전부 배열에 입력
 		getline(fin, arr[i]);
 	}
 	int i = 0;
 	while (i != shortnum) {
+		if (arr[i] == "NULL") { //이미 입력했을 경우 넘기기
+			i++;
+			continue;
+		}
 		bst.insertleft(arr[i]);
 		if (bst.getleftsuccess() == true) { //왼쪽 삽입 성공
 			cout << "input left" << endl;
-			memmove(arr + i, arr + i + 1, arr->size() - i); //완료된 원소는 삭제
+			arr[i] = "NULL"; //완료된 원소는 삭제
 			i = 0; //다시 0부터 비교
 		}
 		else if (bst.getleftsuccess() == false) { //왼쪽 삽입 실패
 			bst.insertright(arr[i]); //오른쪽에 비교
 			if (bst.getrightsuccess() == true) { //오른쪽 삽입 성공
 				cout << "input right" << endl;
-				memmove(arr + i, arr + i + 1, arr->size() - i); //완료 원소 삭제
+				arr[i] = "NULL"; //완료 원소 삭제
 				i = 0; //다시 0부터 비교
 			}
 			else if (bst.getrightsuccess() == false) { //오른쪽도 삽입 실패
@@ -50,10 +55,10 @@ int main() {
 	}
 	string myDNA = bst.remake();
 	ofstream fout;
+	cout <<"\nresult"<< myDNA << endl;
 	fout.open("myDNA_by_denovo.txt");
 	fout.write(myDNA.c_str(), myDNA.size());
 	return 0;*/
-
 
 	//algoritm2
 	algorithm2 al2(70, 100001000);// string reference = "reference.txt", string shortread = "shortread.txt", "mydna.txt");

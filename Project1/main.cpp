b12e7d469c6f57703ea978a8673e963331f09392
Moonsu9158 <62553959+Moonsu9158@users.noreply.github.com>
2020-06-17 01:00:40 +0900

    denovo방식 메인 변경

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -29,13 +29,19 @@ int main() {
 	tri.reconstruct();
 	tri.compare();*/
 
-	/*BSTree<string> bst;
+	/*
+	BSTree<string> bst;
 	ifstream fin;
 	fin.open("shortread.txt");
 	cout << "shortread 개수 ?";
 	int shortnum;
 	cin >> shortnum;
+	bool leftsuccess;
+	bool rightsuccess;
+	clock_t start, end;
+	double result;
 
+	start = clock();
 	string *arr = new string[shortnum];
 	for (int i = 0; i < shortnum; i++) { //쇼트리드를 전부 배열에 입력
 		getline(fin, arr[i]);
@@ -46,30 +52,33 @@ int main() {
 			i++;
 			continue;
 		}
-		bst.insertleft(arr[i]);
-		if (bst.getleftsuccess() == true) { //왼쪽 삽입 성공
-			cout << "input left" << endl;
-			arr[i] = "NULL"; //완료된 원소는 삭제
-			i = 0; //다시 0부터 비교
+		bst.insert(arr[i]);
+		leftsuccess = bst.getleftsuccess();
+		rightsuccess = bst.getrightsuccess();
+		if (leftsuccess == true) {
+			cout << "왼쪽 삽입 성공" << endl;
+			i = 0;
+			continue;
 		}
-		else if (bst.getleftsuccess() == false) { //왼쪽 삽입 실패
-			bst.insertright(arr[i]); //오른쪽에 비교
-			if (bst.getrightsuccess() == true) { //오른쪽 삽입 성공
-				cout << "input right" << endl;
-				arr[i] = "NULL"; //완료 원소 삭제
-				i = 0; //다시 0부터 비교
-			}
-			else if (bst.getrightsuccess() == false) { //오른쪽도 삽입 실패
-				i++; //i증가 후 비교
-				continue;
-			}
+		else if (leftsuccess == false && rightsuccess == true) {
+			cout << "오른쪽 삽입 성공" << endl;
+			i = 0;
+			continue;
+		}
+		else if (leftsuccess == false && rightsuccess == false) {
+			i++;
+			continue;
 		}
 	}
 	string myDNA = bst.remake();
+	cout << "복원한 dna길이 : " << myDNA.length() << endl;
 	ofstream fout;
-	cout <<"\nresult"<< myDNA << endl;
 	fout.open("myDNA_by_denovo.txt");
 	fout.write(myDNA.c_str(), myDNA.size());
+	end = clock();
+	result = (double)(end - start) / (CLOCKS_PER_SEC);
+	cout << "걸린 시간 : " << result << "초" << endl;
+	return 0;
 	*/
 
 	//algoritm2

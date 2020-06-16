2bca290174328a836b14d3d7fb5418a9b0030aac
Bak Jisoo <48276691+jisoo449@users.noreply.github.com>
2020-06-16 23:27:43 +0900

    시간만 추가하자

--- Project1/algorithm2.h
+++ Project1/algorithm2.h
@@ -2,13 +2,21 @@
 #ifndef _ALGORITHM2_H
 #define _ALGORITHM2_H
 using namespace std;
-
+struct sh {
+	string sh;
+	vector<int> index;
+};
 class algorithm2 {
 	ofstream fout;//fout:mydna
 	ifstream fin1, fin2;//fin1:shortread, fin2: reference 
 	int n, k, d; //n:shortread개수, k:shortread길이, d:허용하는 mismatch 개수
 	string shortread, mydna, reference;
 	int x = 2;
+	sh* idxtable;
+
+	int accuracy;
+	float time;
+
 
 public:
 	algorithm2(int k, int n, string reference = "reference.txt", string shortread = "shortread.txt", string mydna = "mydna.txt");

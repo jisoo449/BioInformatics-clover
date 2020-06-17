3bdbfcb5de407a2fd0f226828de3bb713094872f
Bak Jisoo <48276691+jisoo449@users.noreply.github.com>
2020-06-18 07:54:03 +0900

    .

--- Project1/trivial.cpp
+++ Project1/trivial.cpp
@@ -4,71 +4,81 @@
 #include<iostream>
 #include<istream>
 #include<fstream>
+#include<string>
 #include "trivial.h"
 
 trivial::trivial(int k, int n, int d, string real, string reference, string shortread, string mydna) :reference(reference), shortread(shortread), mydna(mydna) {
-   this->n = n;
-   this->k = k;
-   this->d = d;
-   this->real = real;
+	this->n = n;
+	this->k = k;
+	this->d = d;
+	this->real = real;
 }
 
 void trivial::reconstruct() {
-   start = clock();
+	start = clock();
 
-   fout.open(mydna);
-   fin1.open(shortread);
-   fin2.open(reference);
+	fout.open(mydna);
+	fin1.open(shortread);
+	fin2.open(reference);
 
-   char *str1 = new char[k], *str2 = new char[k];
-   char c;
-   int idx=0,mismatch=0,i=0;
-   while (fin1.getline(str1, k)) {
+	string str1;
+	char *str2 = new char[k + 1];
+	char c;
+	int idx = 0, mismatch = 0, i = 0;
+	while (getline(fin1, str1)) {
+		do {
+			fin2.get(c);
+			if (str1[i] == c) i++;
+			else {
+				mismatch++;
+				i++;
+			}
+			if (mismatch > d) {
+				mismatch = 0;
+				i = 0;
+				fin2.clear();
+				fin2.seekg(++idx, ios::beg);
+			}
+			if (i == k) break;
+		} while (!fin2.eof());
+		if (i == k) {
+			fout.clear();
+			fout.seekp(idx, ios::beg);
+			fout << str1;
+		}
+		i = 0;
+		idx = 0;
+		mismatch = 0;
+	}
 
-      do {
-         fin2.get(c);
-         if (str1[i] == c) i++;
-         else mismatch++;
-         if (mismatch > d) {
-            mismatch = 0;
-            i = 0;
-            fin1.seekg(++idx, ios::beg);
-         }
-      }while (!fin2.eof()||i<k);
+	fin1.close();
+	fin2.close();
+	fout.close();
 
-      fout.seekp(idx, ios::beg);
-      fout << str1;
-      i = 0;
-      idx = 0;
-      mismatch = 0;
-   }
-
-   fin1.close();
-   fin2.close();
-   fout.close();
-
-   time = (float)(clock() - start) / CLOCKS_PER_SEC;
+	time = (float)(clock() - start) / CLOCKS_PER_SEC;
 }
 
 void trivial::compare() {
-   int correct = 0,size=0;
-   char c1, c2;
-   fin1.open(real);
-   fin2.open(mydna);
+	int correct = 0, size = 0;
+	char c1, c2;
+	fin1.open(real);
+	fin2.open(mydna);
+	fin1.seekg(0, ios::end);
+	size = fin1.tellg();
+	fin1.clear();
+	fin1.seekg(0, ios::beg);
+	fin2.seekg(0, ios::beg);
+	do {
+		fin1.get(c1);
+		fin2.get(c2);
+		if (c1 == c2)correct++;
 
-   do {
-      fin1.get(c1);
-      fin2.get(c2);
-      if (c1 == c2)correct++;
-      size++;
-   } while (!fin1.eof());
+	} while (!fin1.eof() || !fin2.eof());
+	accuracy = ((double)correct / (double)size) * 100;
 
-   fin1.seekg(0, ios::beg);
-   accuracy = (correct / size) * 100;
-
-   //cout << "걸린 시간: " << time << endl;
-   cout << "정확도: " << accuracy << "%" << endl;
-   cout << "일치한 문자 개수: " << correct << endl;cout << ", 전체 dna 길이: " << fin1.tellg() << endl;
-   fin1.close();
-   fin2.close();
+	cout << "걸린 시간: " << time << endl;
+	cout << "정확도: " << accuracy << "%" << endl;
+	cout << "일치한 문자 개수: " << correct << ", 전체 dna 길이: " << size << endl;
+	fin1.close();
+	fin2.close();
 }
